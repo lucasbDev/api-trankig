986694e783b7b0f11acaf69b152eaa2d02e25882
lucasBdev <luccacpm@gmail.com>
2023-10-15 17:12:22 -0300

    feat: playerDTO

---
+++ src/players/interfaces/player.ts
@@ -0,0 +1,9 @@
+export interface Player {
+  readonly _id: string;
+  readonly _phoneNumber: string;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  positionRanking: string;
+  urlPhotoPlayer: string;
+}
